ce9f9cdc8e0b72557f56e29f083f129ade38c8f8
Frank Moley <frank.moley@garmin.com>
2017-09-28 08:40:28 -0500

    Modifying service

--- code/person-service/src/main/java/com/frankmoley/talks/cloud/personservice/PersonController.java
+++ code/person-service/src/main/java/com/frankmoley/talks/cloud/personservice/PersonController.java
@@ -1,13 +1,8 @@
 package com.frankmoley.talks.cloud.personservice;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -27,15 +22,5 @@ public class PersonController {
         return this.repository.findAll();
     }
 
-    @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public Person addPerson(@RequestBody Person person){
-        return this.repository.save(person);
-    }
-
-    @GetMapping("/{id}")
-    public Person getPerson(@PathVariable long id){
-        return this.repository.findOne(id);
-    }
 
 }
